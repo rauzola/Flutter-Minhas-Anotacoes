735907ee40b6314c4cd55c0f3e6e5f79aae68742
rauzola <raul_sigoli@hotmail.com>
2020-02-15 18:26:41 -0300

    Flutter-0.0.1

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\raul_\Documents\Nova Pasta\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\raul_\Desktop\Flutter\minhas_anotacoes"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\raul_\Documents\Nova Pasta\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
